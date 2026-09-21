yaap/frameworks_base

base: Allow configuring wake sensors on ambient [1/2]

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -242,6 +242,9 @@ public class SecureSettings {
         Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_ALLOW_AMBIENT,
+        Settings.Secure.DOZE_TAP_GESTURE_ALLOW_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_ALLOW_AMBIENT,
         Settings.Secure.EXTRA_DIM_AUTO_MODE,
         Settings.Secure.EXTRA_DIM_AUTO_TIME,
     };

amanrajOO7/derp_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -363,6 +363,9 @@ public class SecureSettings {
         Settings.Secure.DISPLAY_COLOR_BALANCE_GREEN,
         Settings.Secure.DISPLAY_COLOR_BALANCE_BLUE,
         Settings.Secure.DISPLAY_COLOR_TEMPERATURE,
-        Settings.Secure.DISPLAY_COLOR_SATURATION
+        Settings.Secure.DISPLAY_COLOR_SATURATION,
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_ALLOW_AMBIENT,
+        Settings.Secure.DOZE_TAP_GESTURE_ALLOW_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_ALLOW_AMBIENT
     };
 }